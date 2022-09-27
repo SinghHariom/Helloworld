7f0aeeb1064d0f249d77a1387786a024abcdd01b
SinghHariom <hs625558@gmail.com>
2022-09-27 15:10:31 +0530

    Added 1 line

--- Helloworld/Program.cs
+++ Helloworld/Program.cs
@@ -11,6 +11,8 @@ namespace Helloworld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            Console.WriteLine("Hello Duniya");
+
         }
     }
 }
